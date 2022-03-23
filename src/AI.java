0b347dd6e0d6ce1efa76acd9529d889986ea8862
palmoto1 <august.palm.music@gmail.com>
2022-03-23 14:52:24 +0100

    removed uneccesary stuff

--- src/AI.java
+++ src/AI.java
@@ -93,17 +93,17 @@ public class AI {
                 boardHandler.doMove(move, 0); // undo the move so it dont affect the actual game
 
                 if (player == maxPlayerID) {
-                    alpha = Math.max(alpha, evaluation); // update alpha
+                    alpha = Math.max(alpha, evaluation); 
                     if (evaluation > bestEvaluation) {
                         bestEvaluation = evaluation;
-                        optimal = move; // the current move has the current best evaluation for max player
+                        optimal = move;
                     }
 
                 } else { // min player
-                    beta = Math.min(beta, evaluation); // update beta
+                    beta = Math.min(beta, evaluation);
                     if (evaluation < bestEvaluation) {
                         bestEvaluation = evaluation;
-                        optimal = move; // the current move has the current best evaluation for min player
+                        optimal = move;
                     }
                 }
 
